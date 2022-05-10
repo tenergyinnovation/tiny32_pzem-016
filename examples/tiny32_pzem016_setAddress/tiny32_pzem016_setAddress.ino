9eb958bfb069fa1b154b50cce33ef09e09aee0a0
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-05-10 16:29:48 +0700

    Rev1.1
    Add PZEM_016_begin

--- examples/tiny32_pzem016_setAddress/tiny32_pzem016_setAddress.ino
+++ examples/tiny32_pzem016_setAddress/tiny32_pzem016_setAddress.ino
@@ -4,9 +4,10 @@
  * Hardware     :     tiny32         
  * Author       :     Tenergy Innovation Co., Ltd.
  * Date         :     14/04/2022
- * Revision     :     1.1
+ * Revision     :     1.2
  * Rev1.0       :     Original
  * Rev1.1       :     Add Relay On, Show Library version  
+ * Rev1.2       :     Add PZEM_016_begin
  * website      :     http://www.tenergyinnovation.co.th
  * Email        :     admin@innovation.co.th
  * TEL          :     +66 82-380-3299
@@ -24,6 +25,7 @@ void setup()
 {
   Serial.begin(115200);
   Serial.printf("\r\n**** tiny32_pzem016_setAddress ****\r\n");
+  mcu.PZEM_016_begin(RXD3,TXD3);
   mcu.Relay(1);
   vTaskDelay(3000);
   mcu.buzzer_beep(2); //buzzer 2 beeps
