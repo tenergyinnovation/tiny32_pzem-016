688cbc189c28daf9f898907e3de021147d1d47e2
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-07-13 13:36:24 +0700

    Add tiny32_v3 library for Example code

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # tiny32_pzem-016
 tiny32 library interface to ZEM-016 MODBUS RTU(AC Power Energy Meter)
 
-* Before run this example, you must install tiny32 library first at below link *
-https://github.com/tenergyinnovation/tiny32
+*** Before run this example, you must install tiny32 library first at below link ***
+
+1) for tiny32_v3 board
+git clone https://github.com/tenergyinnovation/tiny32_v3.git
+
+or
+
+2) for tiny32 board
+git clone https://github.com/tenergyinnovation/tiny32.git
--- examples/tiny32_pzem016_resetEnergy/tiny32_pzem016_resetEnergy.ino
+++ examples/tiny32_pzem016_resetEnergy/tiny32_pzem016_resetEnergy.ino
@@ -4,16 +4,19 @@
  * Hardware     :     tiny32         
  * Author       :     Tenergy Innovation Co., Ltd.
  * Date         :     14/04/2022
- * Revision     :     1.2
+ * Revision     :     1.3
  * Rev1.0       :     Original
  * Rev1.1       :     Add Relay On, Show Library version  
  * Rev1.2       :     Add PZEM_016_begin
+ * Rev1.3       :     Add tiny32_v3
  * website      :     http://www.tenergyinnovation.co.th
  * Email        :     admin@innovation.co.th
  * TEL          :     +66 82-380-3299
  ***********************************************************************/
 #include <Arduino.h>
-#include <tiny32.h>
+//#include <tiny32.h> //for tiny32 original board
+#include <tiny32_v3.h> //for tiny32 v3
+
 
 tiny32 mcu; //define object
 
@@ -23,7 +26,7 @@ void setup()
 {
   Serial.begin(115200);
   Serial.printf("\r\n**** tiny32_pzem016_resetEnergy ****\r\n");
-  mcu.PZEM_016_begin(RXD3,TXD3);
+  mcu.PZEM_016_begin(RXD2,TXD2);
   mcu.Relay(1);
   vTaskDelay(3000);
   mcu.buzzer_beep(2); //buzzer 2 beeps
